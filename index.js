f1f02205bb6edad63b421ae77c1a6fac62d885cf
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-05-18 02:20:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function greet(timeString){
   if (hours < 12) {
     return "Good Morning"
   }else if (hours <= 17) {
-    return " Good Afternoon"
+    return "Good Afternoon"
   }else  {
     return "Good Evening"
   }
